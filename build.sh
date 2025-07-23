145bd740a33e7fe698d0f0e0e9c64af21de69644
Joksiel <siel3@yahoo.com>
2025-07-23 18:36:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+
 echo -e "\e[32minstalling packages...\e[0m"
 sudo apt update && sudo apt install nasm binutils -y
-echo "Starting builder"
 for app_dir in apps/*; do
-    if [[ -d "$app_dir" && -f "$app_dir/build.sh" ]]; then
+    if [[ -d "$app_dir" && -f "$app_dir/content.asm" ]]; then
         echo "Building $(basename "$app_dir")..."
-        (cd "$app_dir" && bash build.sh)
+        (cd "$app_dir")
+        nasm -f elf64 content.asm -o content.o
+        ld asmination.o -o $app_dir
     fi
 done
-echo "Builds completed"
+echo "Completed"
